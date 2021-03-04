907924bf976106ab6e111589f21e9177a89f941c
vikas reddy <vikasreddy1211@gmail.com>
2021-03-04 21:30:46 +0530

    inheritance

--- src/com/newprojectsonoops/Main.java
+++ src/com/newprojectsonoops/Main.java
@@ -1,25 +1,27 @@
-package com.newprojectsonoops;///this program is for encapsulation
-class student{
-    private String name;
-    private int rollno;
-    public void setRollno(int rollno) {
-        this.rollno = rollno;
+package com.newprojectsonoops;
+class calc{
+    int z;
+    public void add(int a,int b){
+        z=a+b;
+        System.out.println("this is the output of addition"+" "+z);
     }
-    public void setName(String name) {
-        this.name = name;
+    public void sub(int a,int b){
+        z=a-b;
+        System.out.println("this is the output of subtraction"+" "+z);
     }
-    public String getName() {
-        return name;
-    }
-    public int getRollno() {
-        return rollno;
+}
+class advcalc extends calc{
+    public void mul(int a,int b){
+        z=a*b;
+        System.out.println("this is the output of mul"+" "+z);
     }
 }
 public class Main {
-    public static void main(String[] args){
-        student obj=new student();
-        obj.setName("vikas");
-        obj.setRollno(425);
-        System.out.println(obj.getName()+":"+obj.getRollno());
+    public static void main(String[] args) {
+        advcalc obj1=new advcalc();
+        obj1.add(5,6);
+        obj1.sub(10,2);
+        obj1.mul(10,5);
+
     }
 }
